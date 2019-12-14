e7cb6a14fd4fca70d8e10429181dfbcf56ca3401
Peter-cloud-web <petergichia165@gmail.com>
2019-12-14 18:45:05 +0300

    Build on diffrent drop down menus for diffrent pizza mrnu,topings and sizes

--- js/script.js
+++ js/script.js
@@ -1,19 +1,9 @@
-$(document).ready(function(){
-  $('buttton#dropbtn1').click(function(){
-    $("button#dropdown2").show();
-    $("button#dropdown2").hide();
-  });
-  $('#dropbtn1').click(function(){
-    $("buttton#dropdown2").show();
-    $("button#dropdown2").hide();
-  });
-});
-$(document).ready(function(){
-  $("#hide").click(function(){
-    $("button#haha").hide();
-  });
-   $("#hide").click(function(){
-    $("button#haha").show();
-});
 
-});
+ $(document).ready(function() {
+     $("#pizza").click(function () {
+$( ".men_ex" ).toggle({
+        duration: 3000,
+    });
+  });  
+  });
+  
