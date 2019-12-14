b73913bd5b25276dd574b8b0eceb81d669ea96c1
Peter-cloud-web <petergichia165@gmail.com>
2019-12-14 16:35:22 +0300

    Hide and show properties in jQuery

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $('buttton#dropbtn1').click(function(){
+    $("button#dropdown2").show();
+    $("button#dropdown2").hide();
+  });
+  $('#dropbtn1').click(function(){
+    $("buttton#dropdown2").show();
+    $("button#dropdown2").hide();
+  });
+});
+$(document).ready(function(){
+  $("#hide").click(function(){
+    $("button#haha").hide();
+  });
+   $("#hide").click(function(){
+    $("button#haha").show();
+});
+
+});
