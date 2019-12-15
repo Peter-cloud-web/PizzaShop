2ac3f3d6649d1c5f6636e7d35ca3875b87c78a55
Peter-cloud-web <petergichia165@gmail.com>
2019-12-15 08:25:57 +0300

    Animate the menu for better transitions

--- js/script.js
+++ js/script.js
@@ -3,6 +3,41 @@
      $("#pizza").click(function () {
 $( ".men_ex" ).toggle({
         duration: 3000,
+
+    });
+$( ".size" ).hide({
+          duration: 3000,
+    });
+$( ".men_top" ).hide({
+        duration: 3000,
+
+    });
+  });  
+
+     $("#Crust").click(function () {
+$( ".men_top" ).toggle({
+        duration: 3000,
+    });
+$( ".size" ).hide({
+          duration: 3000,
+    });
+$( ".men_ex" ).hide({
+        duration: 3000,
+
+    });
+  }); 
+
+     $("#Sizes").click(function () {
+$( ".size" ).toggle({
+        duration: 3000,
+
+    });
+$( ".men_ex" ).hide({
+        duration: 3000,
+
+    });
+$( ".men_top" ).toggle({
+        duration: 3000,
     });
   });  
   });
