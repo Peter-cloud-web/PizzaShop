df78e5779ae7d08f0778d481e55f758dde53f091
Peter-cloud-web <petergichia165@gmail.com>
2019-12-16 12:56:46 +0300

    Add order prompt to select order location and price

--- js/script.js
+++ js/script.js
@@ -34,43 +34,43 @@ $(document).ready(function() {
         var totalPrice = (inputForUserOrder.newSize + inputForUserOrder.newCrust +inputForUserOrder.newToppings) *  inputForUserOrder.newQuantity;        
             alert("Your total price is Ksh :"  +totalPrice);
 
-            // if (totalPrice > 0) {
-            // alert("Your order is = ksh" + totalPrice);
-            // var delivery = confirm(
-            // "For home delivery, an additional fee of 150 will be inccured. Would you like to proceed?");
-            // if (delivery === true) {
-            //     for (;;) {
-            //         var location = prompt(
-            //         "Select a location for delivery: "
-            //         );
-            //         if (location !== "") {
-            //         alert(
-            //             "Your order will be delivered here: " +
-            //             location +
-            //             ". Thank you! Order again soon"
-            //         );                    break;
-            //         } else {
-            //         alert(
-            //             "Please enter a valid location to have your order delivered!"
-            //         );
-            //         }
-            //     }                alert(
-            //         "Your order is = ksh" +
-            //         totalPrice +
-            //         " + ksh100 delivery fee."
-            //     );
-            //     } else {
-            //     alert(
-            //         "Thank you! Your order has been processed, pick it within 72hrs or order."
-            //     );
-            //     alert(
-            //         "Your total order is = ksh" + totalPrice
-            //     );
-            //     }
-            //     $("#placeOrder").reset();
-            // } else {
-            //     alert("Please fill in all the valid fields for an order");
-            // }        event.preventDefault();
+            if (totalPrice > 0) {
+            alert("Your order is = ksh" + totalPrice);
+            var delivery = confirm(
+            "For home delivery, an additional fee of 150 will be inccured. Would you like to proceed?");
+            if (delivery === true) {
+                for (;;) {
+                    var location = prompt(
+                    "Select a location for delivery: "
+                    );
+                    if (location !== "") {
+                    alert(
+                        "Your order will be delivered here: " +
+                        location +
+                        ". Thank you! Order again soon"
+                    );                    break;
+                    } else {
+                    alert(
+                        "Please enter a valid location to have your order delivered!"
+                    );
+                    }
+                }                alert(
+                    "Your order is = ksh" +
+                    totalPrice +
+                    " + ksh100 delivery fee."
+                );
+                } else {
+                alert(
+                    "Thank you! Your order has been processed, pick it within 72hrs or order."
+                );
+                alert(
+                    "Your total order is = ksh" + totalPrice
+                );
+                }
+                $("#placeOrder").reset();
+            } else {
+                alert("Please fill in all the valid fields for an order");
+            }        event.preventDefault();
     });
   });
 
