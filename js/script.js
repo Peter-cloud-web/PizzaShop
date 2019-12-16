eb2a376ed94344113782b627828e8f2f539747a3
Peter-cloud-web <petergichia165@gmail.com>
2019-12-16 09:54:49 +0300

    Add selected items to a table

--- js/script.js
+++ js/script.js
@@ -1,60 +1,104 @@
 
- $(document).ready(function() {
+ 
+ $(document).ready(function()   {
      $("#Order").click(function () {
-$( "#pizza" ).toggle({
-        duration: 3000,
-    
-    });
-$( "#Sizes" ).toggle({
-          duration: 3000,
-    });
-$( "#Crust" ).toggle({
-        duration: 3000,
 
-    });
-  });  
-
-
-     $("#pizza").click(function () {
 $( ".men_ex" ).toggle({
         duration: 3000,
 
     });
-$( ".size" ).hide({
-          duration: 3000,
-    });
-$( ".men_top" ).hide({
-        duration: 3000,
-
-    });
-  });  
-
-     $("#Crust").click(function () {
 $( ".men_top" ).toggle({
         duration: 3000,
     });
-$( ".size" ).hide({
-          duration: 3000,
-    });
-$( ".men_ex" ).hide({
-        duration: 3000,
-
-    });
-  }); 
-
-     $("#Sizes").click(function () {
 $( ".size" ).toggle({
         duration: 3000,
+  });  
+ });
+});
 
+  $(document).ready(function(){
+    $("#piz1").click(function () {
+    $( "#icon1" ).toggle({
     });
-$( ".men_ex" ).hide({
-        duration: 3000,
+    event.preventDefault();
+  });
+     $("#piz2").click(function () {
+    $( "#icon2" ).toggle({
+    });
+    event.preventDefault();
+  });
+     $("#piz3").click(function () {
+    $( "#icon3" ).toggle({
+    });
+    event.preventDefault();
+  });
+     $("#piz4").click(function () {
+    $( "#icon4" ).toggle({
+    });
+    event.preventDefault();
+  });
+     $("#piz5").click(function () {
+    $( "#icon5" ).toggle({
+    });
+    event.preventDefault();
+  });
 
+
+
+
+
+     $("#crus1").click(function () {
+    $( "#icon6" ).toggle({
     });
-$( ".men_top" ).toggle({
-        duration: 3000,
+    event.preventDefault();
+  });
+     $("#crus2").click(function () {
+    $( "#icon7" ).toggle({
     });
-  }); 
-    }); 
+    event.preventDefault();
+  });
+     $("#crus3").click(function () {
+    $( "#icon8" ).toggle({
+    });
+    event.preventDefault();
+  });
+     $("#crus4").click(function () {
+    $( "#icon9" ).toggle({
+    });
+    event.preventDefault();
+  });
+     $("#crus5").click(function () {
+    $( "#icon10" ).toggle({
+    });
+    event.preventDefault();
+  });
+
+
+
+
+     $("#size1").click(function () {
+    $( "#icon11" ).toggle({
+    });
+    event.preventDefault();
+  });  
+      $("#size2").click(function () {
+    $( "#icon12" ).toggle({
+    });
+    event.preventDefault();
+  });
+   $("#size3").click(function () {
+    $( "#icon13" ).toggle({
+    });
+    event.preventDefault();
+  });
+});
+
+   $(document).ready(function(){
+    $("li#lit").click(function () {
+    var val = $("#lit").text
+      alert(val);
+  });
+});
+
 
   
